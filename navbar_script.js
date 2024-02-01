c26e3f24b879fa90e7ef650781e09c242bd1e4a3
Juan Estrella <juan.lestrella03@gmail.com>
2024-02-01 11:04:49 -0500

    add: add functionality to the hamburger menu using css and javascript

--- navbar_script.js
+++ navbar_script.js
@@ -1,3 +1,20 @@
+
+
+const hamburger = document.querySelector(".hamburger");
+const navBar = document.querySelector(".nav_bar");
+const navList = document.querySelector(".nav_list");
+
+// console.log(hamburger);
+// if (hamburger == null){document.write("null hamburger");}
+hamburger.addEventListener("click", mobileMenu);
+
+
+function mobileMenu() {
+    // hamburger.classList.toggle("activate");
+    navBar.classList.toggle("activate");
+    navList.classList.toggle("activate");
+}
+
 function printFunction(){
     document.writeln("Hello World");
 }
