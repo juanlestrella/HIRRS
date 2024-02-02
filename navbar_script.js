79512bb6069a312b42d6a8757510671f9f20aae2
unknown <juan.lestrella03@gmail.com>
2024-02-02 10:56:06 -0500

    Added: Mobile Menu: submenu. Fix: design of the navigation

--- navbar_script.js
+++ navbar_script.js
@@ -3,18 +3,19 @@
 const hamburger = document.querySelector(".hamburger");
 const navBar = document.querySelector(".nav_bar");
 const navList = document.querySelector(".nav_list");
+const mainMenu = document.querySelectorAll(".main_menu")
+const submenu = document.querySelectorAll(".submenu");
 
-// console.log(hamburger);
-// if (hamburger == null){document.write("null hamburger");}
+ // call the  mobileMenu when the hamburger menu is clicked
 hamburger.addEventListener("click", mobileMenu);
 
-
+/*
+    1.) call the activate class for navBar and navList
+    2.) hide the submenu so that the grid layout doesn't register all of them initially
+*/
 function mobileMenu() {
-    // hamburger.classList.toggle("activate");
     navBar.classList.toggle("activate");
     navList.classList.toggle("activate");
-}
+    submenu.forEach( element => element.classList.toggle("hide"));
 
-function printFunction(){
-    document.writeln("Hello World");
 }
